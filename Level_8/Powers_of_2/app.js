1d38171297673dfa207dc1a364bd9dda6573ee7c
cperalt <cperalt@github.com>
2024-05-20 13:47:09 -0400

    Created Level 8 folder and first challenge added

---
+++ Level_8/Powers_of_2/app.js
@@ -0,0 +1,24 @@
+
+// Challenge: 
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2
+// with the exponent ranging from 0 to n ( inclusive ).
+
+function powersOfTwo(n) {
+
+    //create empty array
+    let arr = [];
+
+    //Iterate from 0 to n, squaring each number and pushing each into the array
+    for (let i = 0; i <= n; i++) {
+        arr.push(Math.pow(2, i));
+    }
+
+    //Return the array
+    return arr;
+}
+
+//Test Cases
+console.log(powersOfTwo(2));
+console.log(powersOfTwo(4));
+
